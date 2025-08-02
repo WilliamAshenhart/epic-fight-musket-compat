9cb4739866d209142556a3eaaf0bb3be1bf77d82
BuildTools <unconfigured@null.spigotmc.org>
2025-08-02 23:10:17 +0400

    - removed unnecessary stuff

--- src/main/java/com/ashenhart/epic_fight_musket_compat/mixin/common/MixinRangedGunAttackGoal.java
+++ src/main/java/com/ashenhart/epic_fight_musket_compat/mixin/common/MixinRangedGunAttackGoal.java
@@ -1,36 +1,28 @@
 package com.ashenhart.epic_fight_musket_compat.mixin.common;
 
-import ewewukek.musketmod.GunItem;
-import ewewukek.musketmod.RangedGunAttackGoal;
-import net.minecraft.world.InteractionHand;
+import ewewukek.musketmod.*;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
 import yesman.epicfight.world.capabilities.EpicFightCapabilities;
 import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
 
 @Mixin(value = RangedGunAttackGoal.class)
-public abstract class MixinRangedGunAttackGoal {
-
+public class MixinRangedGunAttackGoal {
     @Redirect(
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/monster/Monster;getUseItem()Lnet/minecraft/world/item/ItemStack;"
+                    target = "Lnet/minecraft/world/entity/monster/Monster;releaseUsingItem()V"
             ),
             method = "tick()V"
     )
-    private ItemStack epicfight$tick(Monster self) {
-        self.startUsingItem(InteractionHand.MAIN_HAND);
+    private void epicfight$tick(Monster self) {
+        self.releaseUsingItem();
 
-        EpicFightCapabilities.getUnparameterizedEntityPatch((Entity)self, LivingEntityPatch.class).ifPresent(livingEntityPatch -> {
-            livingEntityPatch.playShootingAnimation();
+        EpicFightCapabilities.getUnparameterizedEntityPatch((Entity)self, LivingEntityPatch.class).ifPresent(entitypatch -> {
+            entitypatch.playShootingAnimation();
         });
-        return null;
     }
 }
