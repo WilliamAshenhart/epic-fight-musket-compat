c6ee078d1033b43fb01f68853ad508fd4e2768ef
BuildTools <unconfigured@null.spigotmc.org>
2025-11-27 00:06:36 +0400

    - Attempting to re-introduce the dual pistol and fix some bugs

--- src/main/java/com/ashenhart/epic_fight_musket_compat/world/capabilities/item/PistolCapability.java
+++ src/main/java/com/ashenhart/epic_fight_musket_compat/world/capabilities/item/PistolCapability.java
@@ -1,6 +1,7 @@
 package com.ashenhart.epic_fight_musket_compat.world.capabilities.item;
 
 import com.ashenhart.epic_fight_musket_compat.gameassets.MusketAnimations;
+import com.google.common.collect.Maps;
 import ewewukek.musketmod.GunItem;
 import ewewukek.musketmod.PistolItem;
 import net.minecraft.sounds.SoundEvent;
@@ -12,7 +13,7 @@ import yesman.epicfight.api.animation.AnimationManager;
 import yesman.epicfight.api.animation.LivingMotion;
 import yesman.epicfight.api.animation.LivingMotions;
 import yesman.epicfight.api.animation.types.AttackAnimation;
-import yesman.epicfight.api.animation.types.MountAttackAnimation;
+import yesman.epicfight.api.animation.types.StaticAnimation;
 import yesman.epicfight.gameasset.Animations;
 import yesman.epicfight.gameasset.EpicFightSounds;
 import yesman.epicfight.particle.EpicFightParticles;
@@ -25,16 +26,43 @@ import yesman.epicfight.world.capabilities.item.RangedWeaponCapability;
 import yesman.epicfight.world.capabilities.item.Style;
 
 import java.util.List;
+import java.util.Map;
 
 public class PistolCapability extends RangedWeaponCapability {
-    private List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> attackMotion;
-    private List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> mountAttackMotion;
+    private final List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> mainAttackMotion;
+    private final List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> dualWieldAttackMotion;
+    private final List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> mountAttackMotion;
+
+    private final Map<LivingMotion, AnimationManager.AnimationAccessor<? extends StaticAnimation>> dualWieldLivingMotions;
+
     protected PistolCapability(CapabilityItem.Builder builder) {
         super(builder);
 
-        this.attackMotion = List.of(MusketAnimations.PISTOL_AUTO_1, MusketAnimations.PISTOL_AUTO_2, MusketAnimations.PISTOL_AUTO_3, MusketAnimations.PISTOL_DASH, yesman.epicfight.gameasset.Animations.SWORD_AIR_SLASH);
+        this.mainAttackMotion = List.of(MusketAnimations.PISTOL_AUTO_1, MusketAnimations.PISTOL_AUTO_2, MusketAnimations.PISTOL_AUTO_3, MusketAnimations.PISTOL_DASH, yesman.epicfight.gameasset.Animations.SWORD_AIR_SLASH);
+        this.dualWieldAttackMotion = List.of(MusketAnimations.DUAL_PISTOL_AUTO1, MusketAnimations.DUAL_PISTOL_AUTO2, MusketAnimations.DUAL_PISTOL_DASH, MusketAnimations.DUAL_PISTOL_AIRSLASH);
         this.mountAttackMotion = List.of(Animations.SWORD_MOUNT_ATTACK);
+
+        this.dualWieldLivingMotions = Maps.newHashMap(this.rangeAnimationModifiers);
+
+        this.dualWieldLivingMotions.put(LivingMotions.IDLE, MusketAnimations.HOLD_DUAL_PISTOL);
+        this.dualWieldLivingMotions.put(LivingMotions.RUN, MusketAnimations.RUN_DUAL_PISTOL);
     }
+
+    @Override
+    public Map<LivingMotion, AnimationManager.AnimationAccessor<? extends StaticAnimation>> getLivingMotionModifier(LivingEntityPatch<?> playerdata, InteractionHand hand) {
+        if (hand == InteractionHand.MAIN_HAND) {
+            Style currentStyle = this.getStyle(playerdata);
+
+            if (currentStyle == Styles.TWO_HAND) {
+                return this.dualWieldLivingMotions;
+            }
+
+            return this.rangeAnimationModifiers;
+        }
+
+        return super.getLivingMotionModifier(playerdata, hand);
+    }
+
 
     @Override
     public SoundEvent getHitSound() {
@@ -48,12 +76,18 @@ public class PistolCapability extends RangedWeaponCapability {
 
     @Override
     public List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> getAutoAttackMotion(PlayerPatch<?> playerpatch) {
-        return this.attackMotion;
+        Style currentStyle = this.getStyle(playerpatch);
+
+        if (currentStyle == Styles.TWO_HAND) {
+            return this.dualWieldAttackMotion;
+        }
+        return this.mainAttackMotion;
     }
 
     public List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> getMountAttackMotion() {
         return this.mountAttackMotion;
     }
+
 
 
     @Override
@@ -70,5 +104,18 @@ public class PistolCapability extends RangedWeaponCapability {
             return Styles.TWO_HAND;
         }
         return Styles.ONE_HAND;
+    }
+
+    @Override
+    public boolean checkOffhandValid(LivingEntityPatch<?> entityPatch) {
+        ItemStack offhandItem = entityPatch.getOriginal().getOffhandItem();
+        CapabilityItem itemCap = EpicFightCapabilities.getItemStackCapability(offhandItem);
+        boolean isPistol = itemCap.getWeaponCategory() == MusketWeaponCategories.PISTOL;
+        return isPistol || !(offhandItem.getItem() instanceof SwordItem || offhandItem.getItem() instanceof DiggerItem);
+    }
+
+    @Override
+    public boolean canHoldInOffhandAlone() {
+        return true;
     }
 }
